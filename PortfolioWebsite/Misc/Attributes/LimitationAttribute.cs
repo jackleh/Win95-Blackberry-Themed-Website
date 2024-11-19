563d51144ea9ef1560588f6965a3bc584c18933f
Jack Lehman <jackl@lehmidt.com>
2024-11-18 22:57:04 -0800

    More Configs + Aligned Intro Animation

---
+++ PortfolioWebsite/Misc/Attributes/LimitationAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+sealed class LimitationAttribute(string message) : Attribute
+{
+    public string Message { get; } = message;
+}
--- PortfolioWebsite/Models/SiteConfig.cs
+++ PortfolioWebsite/Models/SiteConfig.cs
@@ -2,6 +2,20 @@
 
 public class SiteConfig
 {
-    public int OpeningSectionDurationInMs { get; set; }
-    public string OpeningSectionKeyword { get; set; }
+    public int OpeningSectionHoldDurationInMs { get; set; }
+
+    private int _openingSectionFadeDurationInMs { get; set; }
+    [Limitation("Only works if 1k,1.5k,2k,2.5k,3k from JSON due to how CSS struggles with dynamic values")]
+    public int OpeningSectionFadeDurationInMs
+    {
+        get => _openingSectionFadeDurationInMs;
+        set
+        {
+           int[] allowedValues = [1000, 1500, 2000, 2500, 3000];
+           _openingSectionFadeDurationInMs = allowedValues.Contains(value) 
+               ? value 
+               : allowedValues.OrderBy(x => Math.Abs(x - value)).First();
+        }
+    }
+    public string SiteTitle { get; set; }
 }
