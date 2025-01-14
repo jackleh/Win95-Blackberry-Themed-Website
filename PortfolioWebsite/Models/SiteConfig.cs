7eee00ecd7b9dbb9115fd53c7634ad8f182c9c95
Jack Lehman <jackl@lehmidt.com>
2025-01-13 21:45:11 -0800

    Arrow key menu navigation, selection animation to config

--- PortfolioWebsite/Models/SiteConfig.cs
+++ PortfolioWebsite/Models/SiteConfig.cs
@@ -5,14 +5,30 @@ public class SiteConfig
     public int OpeningSectionHoldDurationInMs { get; set; }
 
     private int _openingSectionFadeDurationInMs { get; set; }
+
     [Limitation("Only works if 1k,1.5k,2k,2.5k,3k from JSON due to how CSS struggles with dynamic values")]
     public int OpeningSectionFadeDurationInMs
     {
         get => _openingSectionFadeDurationInMs;
         set
         {
+            int[] allowedValues = [1000, 1500, 2000, 2500, 3000];
+            _openingSectionFadeDurationInMs = allowedValues.Contains(value)
+                ? value
+                : allowedValues.OrderBy(x => Math.Abs(x - value)).First();
+        }
+    }
+
+    private int _sectionSelectionFlashTimeDurationInMs { get; set; }
+
+    [Limitation("Only works if 1k,1.5k,2k,2.5k,3k from JSON due to how CSS struggles with dynamic values")]
+    public int SectionSelectionFlashTimeDurationInMs
+    {
+        get => _sectionSelectionFlashTimeDurationInMs;
+        set
+        {
            int[] allowedValues = [1000, 1500, 2000, 2500, 3000];
-           _openingSectionFadeDurationInMs = allowedValues.Contains(value) 
+           _sectionSelectionFlashTimeDurationInMs = allowedValues.Contains(value) 
                ? value 
                : allowedValues.OrderBy(x => Math.Abs(x - value)).First();
         }
